039f250519ece37b147083386b6c2bf5e2d1f8a5
Ranjinie-Souria <ranjinie.souria@gmail.com>
2022-11-18 13:59:34 +0100

    creating project adding patient proxy sevice adding assessment class

---
+++ src/main/java/com/mediscreen/CustomProperties.java
@@ -0,0 +1,18 @@
+package com.mediscreen;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomProperties {
+
+    private String apiPatient;
+	public String getApiPatient() {
+		return apiPatient;
+	}
+	public void setApiPatient(String apiPatient) {
+		this.apiPatient = apiPatient;
+	}
+    
+    
+
+}
---
+++ src/main/java/com/mediscreen/model/Assessment.java
@@ -0,0 +1,39 @@
+package com.mediscreen.model;
+
+public class Assessment {
+	private Integer patientId;
+	private int patientAge;
+	private String result;
+	
+	public Assessment(Integer patientId, int patientAge, String result) {
+		this.patientId = patientId;
+		this.patientAge = patientAge;
+		this.result = result;
+	}
+	public Assessment(int patientAge, String result) {
+		this.patientAge = patientAge;
+		this.result = result;
+	}
+	public Assessment() {
+	}
+	public Integer getPatientId() {
+		return patientId;
+	}
+	public void setPatientId(Integer patientId) {
+		this.patientId = patientId;
+	}
+	public int getPatientAge() {
+		return patientAge;
+	}
+	public void setPatientAge(int patientAge) {
+		this.patientAge = patientAge;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+	
+}
